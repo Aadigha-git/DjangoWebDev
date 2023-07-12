e10c059caf2a795c45e52094cc22504e0c806aab
Aadigha-git <ba.aaditya03@gmail.com>
2023-07-12 23:47:15 +0530

    basic html useage with django

--- django_tutorial/mysite/main/urls.py
+++ django_tutorial/mysite/main/urls.py
@@ -4,10 +4,9 @@ from . import views
 # imports view from the current directory we are in
 
 urlpatterns = [
-    path("",views.index, name="index"), 
+    path("", views.home, name="home"), 
     # If we get into the application and then we are on the homepage --> ""
     # go to views.index has name "index"
     # and we serve the http response index
-    path("<int:id>", views.id, name = "id"),
-    path("<str:name>", views.name1, name = "name")
+    path("<int:id>", views.idx, name = "id"),
 ]
--- django_tutorial/mysite/main/views.py
+++ django_tutorial/mysite/main/views.py
@@ -4,14 +4,11 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello World!</h1>")
+def home(response):
+    return render(response, 'main/home.html', {})
 
-def id(response, id):
+def idx(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id = 1)
-    return HttpResponse("<h2>%s</h2><br><p>%s<br>%s</p>" % (ls.name, item.text, item.complete))
-
-def name1(response, name):
-    ls = ToDoList.objects.get(name = name)
-    return HttpResponse("<h3>%s</h3>" % ls.name)
+    #item = ls.item_set.get(id = id)
+    # return HttpResponse("<h2>%s</h2><br><p>%s<br>%s</p>" % (ls.name, item.text, item.complete))
+    return render(response, 'main/base.html', {})
