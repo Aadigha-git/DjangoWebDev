d5eebdca38d87ba49c08d45007e8c090f3b09492
Aadigha-git <ba.aaditya03@gmail.com>
2023-07-14 21:03:04 +0530

    for and if loops in django

--- django_tutorial/mysite/main/views.py
+++ django_tutorial/mysite/main/views.py
@@ -11,4 +11,4 @@ def idx(response, id):
     ls = ToDoList.objects.get(id=id)
     #item = ls.item_set.get(id = id)
     # return HttpResponse("<h2>%s</h2><br><p>%s<br>%s</p>" % (ls.name, item.text, item.complete))
-    return render(response, 'main/base.html', {})
+    return render(response, 'main/list.html', {"ls":ls})
